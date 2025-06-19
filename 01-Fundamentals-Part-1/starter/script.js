6bf661d4526967c4cc292d0d13226393ed31d82e
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2025-06-19 14:40:26 +0200

    First day of working on the course
    next video is section 2: nr 13 Basic operators

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,50 @@
+/*
+let js = "amazing";
+console.log(48 + 8 + 23 - 10);
+
+let firstName = "Anders";
+console.log(firstName);
+
+let PI = 3.1415;
+//By writing in all uppercase letters, we are telling JavaScript that PI is a constant
+
+
+//Assignment task.
+let country = "Norway";
+let continent = "Europe";
+let population = 5.52;
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Anders");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1994;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1994;
+//The value of birthYear cannot be changed once it is declared.
+
+//use const as default unless you are REALLY sure you need to change it.
+//var should be avoided unless you are working with older codebases.
+// Var works pretty much the same as let, but it has some differences in how it handles scope.
