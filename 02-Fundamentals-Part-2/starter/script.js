58f8df70007403464dca3808d36166a640ae01b0
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2025-06-24 21:02:40 +0200

    Course work.
    Next vid is section3: nr 48.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -212,8 +212,8 @@ if (friends.includes("Steven")) {
   console.log("You have a friend called Steven");
 }
 */
-
 /*
+// MY CODE
 function calcTip(billValue) {
   if (billValue >= 50 && billValue <= 300) {
     return billValue * 0.15;
@@ -235,3 +235,180 @@ const totals = [
 
 console.log(totals);
 */
+
+/*
+// Jonas' code
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+// const calcTip = bill => Bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+const bills = [125, 55, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [
+  calcTip(bills[0]) + bills[0],
+  calcTip(bills[1]) + bills[1],
+  calcTip(bills[2]) + bills[2],
+];
+
+console.log(bills, tips, totals);
+*/
+
+/*
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2025 - 1991,
+  job: "Teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+//Does not work. We need to use bracket notation as we did above.
+// console.log(jonas."last" + nameKey)
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+);
+//Here you have to use bracket notation.
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("We don't know that about Jonas.");
+}
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtman";
+console.log(jonas);
+
+//Challenge
+// 'Jonas has 3 friends, and his best friend is called Michael'
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`
+);
+
+*/
+/*
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "Teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2025 - birthYear;
+  // }
+
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2025 - this.birthYear;
+  // },
+  //Writing "this." to make the function point to the jonas object.
+
+  calcAge: function () {
+    this.age = 2025 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    // const canDrive = this.hasDriversLicense
+    //   ? "a driver's license"
+    //   : "not a driver's license";
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.job
+    }, and he has ${this.hasDriversLicense ? "a" : "not a"} driver's license.`;
+  },
+};
+
+console.log(jonas.calcAge());
+// console.log(jonas["calcAge"](jonas.birthYear));
+console.log(jonas);
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+//Challenge
+// 'Jonas is a 46-year old teacher. and he has a(no) driver's license.'
+console.log(jonas.getSummary());
+*/
+
+/*
+CHALLENGE #3
+Let's go back to Mark and John comparing their BMIs!
+
+This time, let's use objects to implement the calculations! Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your tasks:
+
+For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith). Name these objects as mark and john, and their properties exactly as fullName, mass and height.
+
+Create a calcBMI method on each object to calculate the BMI (the same method on both objects). Assign the BMI value to a property called bmi (lowercase), and also return it from the method.
+
+Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+
+
+IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2). */
+
+/*
+const mark = {
+  fullName: "Mark Miller",
+  height: 1.69,
+  mass: 78,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  height: 1.95,
+  mass: 92,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+mark.calcBMI();
+john.calcBMI();
+console.log(
+  mark.bmi > john.bmi
+    ? `${mark.fullName} BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+    : `${john.fullName} BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
+);
+*/
+
+// console.log("Lifting weights repetition 1 💪");
+// console.log("Lifting weights repetition 2 💪");
+// console.log("Lifting weights repetition 3 💪");
+// console.log("Lifting weights repetition 4 💪");
+// console.log("Lifting weights repetition 5 💪");
+// console.log("Lifting weights repetition 6 💪");
+// console.log("Lifting weights repetition 7 💪");
+// console.log("Lifting weights repetition 8 💪");
+// console.log("Lifting weights repetition 9 💪");
+// console.log("Lifting weights repetition 10 💪");
+
+//For loop keeps running while condition is TRUE.
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 💪`);
+}
