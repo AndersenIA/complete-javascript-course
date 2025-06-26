29f8f440a67cc9b3adc2cd0f672982d766ff38a8
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2025-06-26 21:36:45 +0200

    course work

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,29 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+console.log('Hello World!');
+
+const calcAge = birthYear => 2025 - birthYear;
+
+console.log(calcAge(2001));
+
+// CODEWARS
+// function monkeyCount(n) {
+//   const monkeys = [];
+//   for (let i = 1; i < n + 1; i++) {
+//     monkeys.push(i);
+//   }
+//   return console.log(monkeys);
+// }
+
+// monkeyCount(5);
+
+// function monkeyCount(n) {
+//   var monkeys = [];
+//   for (var i = 1; i < n + 1; i++) {
+//     monkeys.push(i);
+//   }
+//   return console.log(monkeys);
+// }
+
+// monkeyCount(5);
