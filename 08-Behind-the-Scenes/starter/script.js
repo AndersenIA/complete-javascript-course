9c562c1bc5c3900f89aa27018afc7de399500f96
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2025-07-01 21:37:59 +0200

    course work

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `Your name is ${firstName}, and you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str); NOT POSSIBLE
+    console.log(millenial); //POSSIBLE BECAUSE VAR
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
