2c0ee16eb42887895d9d0d85f208136fdeebd3e0
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2025-07-20 09:45:56 +0200

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -143,6 +143,7 @@ const round = function () {
 // greeterHey('Steven');
 // greet('Hello')('Jonas');
 
+/*
 const lufthansa = {
   airline: 'Lufthansa',
   iatacode: 'LH',
@@ -163,7 +164,7 @@ lufthansa.book(635, 'John Smith');
 console.log(lufthansa);
 
 const eurowings = {
-  name: 'Eurowings',
+  airline: 'Eurowings',
   iatacode: 'EW',
   bookings: [],
 };
@@ -172,8 +173,143 @@ const book = lufthansa.book;
 
 // DOES NOT WORK
 // book(23, 'Sarah Williams');
+
+//Call method
 book.call(eurowings, 23, 'Sarah Williams');
 console.log(eurowings);
 
 book.call(lufthansa, 239, 'Mary Cooper');
 console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air lines',
+  iatacode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData); // Could use the spread operator instead like below
+
+book.call(swiss, ...flightData);
+console.log(swiss);
+
+// Bind method (manually set the 'this' keyword)
+// book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'Steven Williams');
+
+// 'Partial application' One argument is already predefined
+const bookEW23 = book.bind(eurowings, 23); // This one only needs the name now
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// Have to use the bind method to point to the lufthansa object. If not the this keyword is going to point to the button instead.
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+// Here you have to say "null" because there is no this keyword
+const addVAT = addTax.bind(null, 0.23);
+// const addTax = (value) => value + value * 0.23;
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+
+
+const poll = {
+  question: 'What is your favorite programming language?',
+  options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+  //This generates [0,0,0,0], More in the next section
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get Answer
+    const userInput = prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+    );
+    //Register answer
+    if (userInput >= 0 && userInput <= 3) {
+      this.answers[userInput]++;
+    } else {
+      alert('Wrong input!');
+    }
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+const bonus1 = [5, 2, 3];
+const bonus2 = [1, 5, 3, 9, 6, 1];
+// poll.displayResults.call({ answers: bonus1 }, 'string');
+// poll.displayResults.call({ answers: bonus2 }, 'array');
+*/
