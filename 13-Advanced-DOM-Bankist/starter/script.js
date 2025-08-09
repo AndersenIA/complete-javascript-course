2e22a2aeb133a08835a2b18440bb7c189e0b26ab
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2025-08-09 07:35:30 +0200

    course work

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -36,7 +36,6 @@ document.addEventListener('keydown', function (e) {
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 // Lectures
-
 /*
 // Selecting elements
 console.log(document.documentElement);
@@ -76,4 +75,47 @@ document
     message.remove(); // New way
     // message.parentElement.removeChild(message); // Old way
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+console.log(message.style.color);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.getAttribute('href'));
+console.log(link.href);
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j')
+logo.classList.remove('c', 'j')
+logo.classList.toggle('c')
+logo.classList.contains('c') // Not includes. 
+
+// Dont use Will overwrite all existing classes
+logo.className = 'jonas'
 */
